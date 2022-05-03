2d5eacc83486763c84b98d9028aa21ffef81fc0e
oKrivov <krivovoleg23@gmail.com>
2022-05-03 10:03:51 +0300

    feat: add input, singin, singup components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import "./App.css";
 import Header from "./components/Header/Header";
+import Input from './components/Input/Input';
+import SingIn from './components/Registration/SingIn';
+import SingUp from './components/Registration/SingUp';
 import Search from "./components/Search/Search";
-import SearchIcon from "./search.svg";
 
 function App() {
-  
-
-
 
   return (
     <div className="app">
       <Header />
+      {/* <Input/> */}
+      {/* <SingIn/> */}
+      {/* <SingUp/> */}
       <Search/>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
-import Button from './../Button/Button';
-import s from './Header.module.css';
-
+import Button from "./../Button/Button";
+import s from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <div className={s.header}>
       <a className={s.logo}>
-        <img src="" alt="logo"/>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1226/1226446.png?w=740"
+          alt="logo"
+        />
       </a>
       <div className={s.buttons}>
-        <Button/>
-        <Button/>
+        <Button />
+        <Button />
       </div>
     </div>
   );
